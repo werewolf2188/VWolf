6c63de177104da6e0af4e4ea93424702e0e01d2c
Enrique Ricalde <jenrique50@gmail.com>
2026-04-30 18:31:39 -0500

    Adding ability for shaders to set up properties at the shader level.
    Adding properties in the shader.
    
    Finalizing reading properties from the shader. Making it available for the editor and the material to be re-written.

--- VWolf/src/VWolf/Core/Render/Shader.cpp
+++ VWolf/src/VWolf/Core/Render/Shader.cpp
@@ -8,6 +8,7 @@
 #include "vwpch.h"
 #include "Shader.h"
 
+#include "VWolf/Core/Math/Serialization/VMath.h"
 #include "VWolf/Core/Utils/GenericSerialization.h"
 
 namespace YAML {
@@ -17,11 +18,39 @@ namespace YAML {
     VWOLF_CREATE_CONVERT_GENERIC_ENUM_DECODER(VWolf::DepthFunction, Never, Less, Equal, LEqual, Greater, NotEqual, GEqual, Always)
     VWOLF_CREATE_CONVERT_GENERIC_ENUM_DECODER(VWolf::BlendEquation, Add, Substract, ReverseSubstract, Min, Max)
     VWOLF_CREATE_CONVERT_GENERIC_ENUM_DECODER(VWolf::BlendFunction, Zero, One, SrcColor, InvSrcColor, DstColor, InvDstColor, SrcAlpha, InvSrcAlpha, DstAlpha, InvDstAlpha, Src1Color, InvSrc1Color, Src1Alpha, InvSrc1Alpha, SrcAlphaSat, CnstColor, InvCnstColor, CnstAlpha, InvCnstAlpha)
+    VWOLF_CREATE_CONVERT_GENERIC_ENUM_DECODER(VWolf::PropertyType, Unknown, Integer, Float, Texture2D, Cubemap, Color, Vector)
 
     VWOLF_CREATE_CONVERT_GENERIC_CLASS_DECODER(VWolf::Rasterization)
     VWOLF_CREATE_CONVERT_GENERIC_CLASS_DECODER(VWolf::DepthStencil)
     VWOLF_CREATE_CONVERT_GENERIC_CLASS_DECODER(VWolf::Blend)
     VWOLF_CREATE_CONVERT_GENERIC_CLASS_DECODER(VWolf::Stage)
+    template<>
+    struct convert<VWolf::Property> {
+        static bool decode(const Node& node, VWolf::Property& rhs)
+        {
+            DeserializeFromBoostDescribe(node, rhs);
+            switch(rhs.type) {
+                case VWolf::PropertyType::Integer:
+                    rhs.value = node["value"] ? node["value"].as<uint32_t>() : 0;
+                    break;
+                case VWolf::PropertyType::Float:
+                    rhs.value = node["value"] ? node["value"].as<float>() : 0;
+                    break;
+                case VWolf::PropertyType::Color:
+                case VWolf::PropertyType::Vector:
+                    rhs.value = node["value"] ? node["value"].as<VWolf::Vector4>() : VWolf::Vector4();
+                    break;
+                case VWolf::PropertyType::Texture2D:
+                case VWolf::PropertyType::Cubemap:
+                    rhs.value = node["value"] ? node["value"].as<std::string>() : "";
+                    break;
+                default:
+                    VWOLF_CORE_ASSERT(false, "Shader Property failure: Wrong type");
+                    break;
+            }
+            return true;
+        }
+    };
     VWOLF_CREATE_CONVERT_GENERIC_CLASS_DECODER(VWolf::SubShader)
     VWOLF_CREATE_CONVERT_GENERIC_CLASS_DECODER(VWolf::Settings)
     VWOLF_CREATE_CONVERT_GENERIC_CLASS_DECODER(VWolf::Shader)
