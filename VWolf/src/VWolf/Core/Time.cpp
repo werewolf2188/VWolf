681bacf925b3dd6b4fe7285f327af609d522c459
Enrique Ricalde <jenrique50@gmail.com>
2026-08-10 08:53:59 -0500

    Making Time a scope pointer instead of a Ref. It should only be one.

--- VWolf/src/VWolf/Core/Time.cpp
+++ VWolf/src/VWolf/Core/Time.cpp
@@ -1,10 +1,12 @@
 #include "vwpch.h"
 #include "Time.h"
 
+#include "VWolf/Core/Events/Event.h"
+#include "VWolf/Core/Events/ApplicationEvent.h"
+
 namespace VWolf
 {
-
-	Ref<Time> Time::m_instanceTime = nullptr;
+    Scope<Time> Time::m_instanceTime = nullptr;
 	float Time::m_deltaTime = 0;
 	float Time::m_lastFrame = 0;
 	long long Time::m_frameCount = 0;
@@ -12,13 +14,18 @@ namespace VWolf
     class GenericTime: public Time {
     protected:
        virtual float GetTime() override;
+       virtual void SendAppTick() override;
     };
 
 	float GenericTime::GetTime() {
 		return (float)glfwGetTime();
 	}
 
-    Ref<Time> CreateGenericTime() {
-        return CreateRef<GenericTime>();
+    void GenericTime::SendAppTick() {
+        EventQueue::DefaultQueue->Queue(CreateRef<AppTickEvent>());
+    }
+
+    Scope<Time> CreateGenericTime() {
+        return CreateScope<GenericTime>();
     }
 }
--- VWolf/src/VWolf/Core/Time.h
+++ VWolf/src/VWolf/Core/Time.h
@@ -29,18 +29,20 @@ namespace VWolf {
 			m_deltaTime = time - m_lastFrame;
 			m_lastFrame = time;
 			m_frameCount += 1;
+            m_instanceTime->SendAppTick();
 		}
 
-		static void SetTimeImplementation(Ref<Time> instance) { m_instanceTime = instance; }
+		static void SetTimeImplementation(Scope<Time> instance) { m_instanceTime = std::move(instance); }
 #endif
 	protected:
-		virtual float GetTime() = 0;		
+        virtual float GetTime() = 0;
+        virtual void SendAppTick() = 0;
 	private:
-		static Ref<Time> m_instanceTime;
+		static Scope<Time> m_instanceTime;
 		static float m_deltaTime;
 		static float m_lastFrame;
 		static long long m_frameCount;
 	};
 
-    Ref<Time> CreateGenericTime();
+    Scope<Time> CreateGenericTime();
 }
